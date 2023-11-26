eff9a69ae80c83adbae65c38e2186d4ea552bfc8
Akash <akash@webmavens.com>
2023-11-26 19:41:04 +0530

    learning component,state,routing and dark-light functionlity add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbars';
 import TextForm from './components/TextForm';
-let name = "akash 12";
+import About from './components/About';
+import React , {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// let name = "akash 12";
 function App() {
+  const [mode,setMode]=useState("light");
+  const toggleMode = ()=>{
+      if(mode==="light"){
+        document.body.style.background = "#042743";
+        document.body.style.color = "#fff";
+        var styleclass = document.querySelector('.form-check-input:checked');
+        
+        console.log(styleclass);
+        setMode("dark");
+      }else{
+        document.body.style.background = "white";
+        document.body.style.color = "#000";
+        setMode("light");
+      }
+  }
+
   return (
     <>
-    <Navbar title="React"/>
+    <Router>
+    <Navbar title="React" mode={mode}  toggleMode={toggleMode} />
     <div className="container">
-      <TextForm heading="Convert to uppecase latter"/>
-    </div>
     
+      <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <TextForm heading="Convert to uppecase latter"/>
+          </Route>
+          </Switch>
+    </div>
+    </Router>
     </>
   );
 }
